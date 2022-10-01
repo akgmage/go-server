51ca8cbcecbc72c3be6d474b9dba5a6b5aa1b3d7
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-01 22:26:57 +0530

    return a handler that serves HTTP requests with the contents of the file system rooted at root

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import "net/http"
+
 func main() {
-	
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
 }
