20f3ff8fca80a98404a0d09311ef0acee72bf295
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-01 22:39:42 +0530

    return error if path not /hello

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 )
 
 func hellohandler(w http.ResponseWriter, r *http.Request) {
-	
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
 }
 
 func main() {
