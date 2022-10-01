9e10d45db34b7256466f41eb89a3242f208be320
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-01 22:34:39 +0530

    ListenAndServe if there is no error or log the error

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err:= http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
