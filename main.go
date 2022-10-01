9b3d5878211870c6dfca2a8e859eff96e1a930cb
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-01 22:45:25 +0530

    add formHandler function and parse the form

--- main.go
+++ main.go
@@ -5,6 +5,17 @@ import (
 	"log"
 	"net/http"
 )
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Parseform() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
 
 func hellohandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
