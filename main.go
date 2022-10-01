97b579787363001e9165b38498225e3457136cda
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-01 22:52:30 +0530

    fix typo

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
-func hellohandler(w http.ResponseWriter, r *http.Request) {
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
